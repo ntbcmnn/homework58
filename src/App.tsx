a4b19e717dea4117caf5bf67b75fa876efcf2475
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-05 21:41:03 +0600

    Displayed Modal component, created a button that shows modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Modal from "./UI/Modal/Modal.tsx";
+import { useState } from "react";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const onModalClick = () => {
+    setShowModal((prevState) => !prevState);
+  };
+
+  return (
+    <div className="container m-5">
+      <Modal show={showModal} title="Modal title" onModalClick={onModalClick}>
+        <p className="ps-3">Example static text</p>
+      </Modal>
+      <button className="btn btn-info text-light" onClick={onModalClick}>
+        First task
+      </button>
+    </div>
+  );
+};
 
 export default App;

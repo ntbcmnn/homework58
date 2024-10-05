9b6bd09ef64e7f12da69a128a3d9b8ab3b574a1c
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-05 21:59:25 +0600

    Changed background for the button (first task)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "./App.css";
-import Modal from "./UI/Modal/Modal.tsx";
-import { useState } from "react";
+import './App.css';
+import Modal from './UI/Modal/Modal.tsx';
+import { useState } from 'react';
 
 const App = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -14,7 +14,7 @@ const App = () => {
       <Modal show={showModal} title="Modal title" onModalClick={onModalClick}>
         <p className="ps-3">Example static text</p>
       </Modal>
-      <button className="btn btn-info text-light" onClick={onModalClick}>
+      <button className="btn btn-dark text-white" onClick={onModalClick}>
         First task
       </button>
     </div>

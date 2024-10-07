b50d7f1c686db48c1bc8432ec24e381d201eff7a
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-07 15:47:26 +0600

    1. Added 2 states for alerts to show them by click on the button 2.Created a function, that closes alert, which has onDismiss props 3. Created a function, that shows closed alert 4. Displayed 2 Alert components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import './App.css';
-import Modal from './UI/Modal/Modal.tsx';
-import { useState } from 'react';
+import "./App.css";
+import Modal from "./UI/Modal/Modal.tsx";
+import { useState } from "react";
+import Alert from "./UI/Alert/Alert.tsx";
 
 const App = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [showWarning, setShowWarning] = useState<boolean>(true);
+  const [showSuccess, setShowSuccess] = useState<boolean>(true);
 
-  const onModalClick = () => {
+  const onModalClick = (): void => {
     setShowModal((prevState) => !prevState);
   };
 
+  const closeAlert = (): void => {
+    setShowWarning((prevState) => !prevState);
+  };
+
+  const showAllAlerts = (): void => {
+    setShowWarning(true);
+    setShowSuccess(true);
+  };
+
   return (
-    <div className="container m-5">
+    <div className="container m-5 ">
+      <div className="d-flex justify-content-center gap-4 mb-4">
+        <button type="button" className="btn btn-dark" onClick={onModalClick}>
+          Show modal
+        </button>
+
+        <button type="button" className="btn btn-light" onClick={showAllAlerts}>
+          Show alerts
+        </button>
+      </div>
+
       <Modal show={showModal} title="Modal title" onModalClick={onModalClick}>
         <p className="ps-3">Example static text</p>
       </Modal>
-      <button className="btn btn-dark text-white" onClick={onModalClick}>
-        First task
-      </button>
+
+      {showWarning && (
+        <Alert type="warning" onDismiss={closeAlert}>
+          Warning type alert
+        </Alert>
+      )}
+
+      {showSuccess && <Alert type="success">Success type alert</Alert>}
     </div>
   );
 };
